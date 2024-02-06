016c9b1304728997c3bcf0d07dcacad7ed9e0f1e
dextryz <rvs.hello@pm.me>
2024-02-06 16:41:14 -0500

    add done item integration

--- main.go
+++ main.go
@@ -17,6 +17,45 @@ type handler struct {
 	cfg *todo.Config
 }
 
+func (s *handler) done(w http.ResponseWriter, r *http.Request) {
+
+	id := strings.TrimPrefix(r.URL.Path, "/done/")
+
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	switch r.Method {
+	case http.MethodPost:
+
+		err := todo.Done(context.Background(), s.cfg, "food", id)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Load list from set of relays
+		ctx := context.Background()
+		var tl todo.TodoList
+		err = tl.Load(ctx, s.cfg, "food")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = tmpl.ExecuteTemplate(w, "index.html", tl)
+		if err != nil {
+			fmt.Println("error executing template:", err)
+		}
+
+	default:
+		http.Error(w, "Unsupported method", http.StatusMethodNotAllowed)
+	}
+}
+
 func (s *handler) remove(w http.ResponseWriter, r *http.Request) {
 
 	id := strings.TrimPrefix(r.URL.Path, "/item/")
@@ -30,7 +69,7 @@ func (s *handler) remove(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodDelete:
 
-		err := todo.Delete(context.Background(), s.cfg, id, "food")
+		err := todo.Delete(context.Background(), s.cfg, "food", id)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -103,6 +142,7 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", h.list)
+	mux.HandleFunc("/done/", h.done)
 	mux.HandleFunc("/item/", h.remove)
 
 	fs := http.FileServer(http.Dir("."))
